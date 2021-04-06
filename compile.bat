a0f94160ef5c14daf9a6a20d45c6c9daf7935f96
plackyhacker <42491100+plackyhacker@users.noreply.github.com>
2021-04-06 21:01:13 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-cl.exe /nologo /Ox /MT /W0 /GS- /DNDEBUG /Tc *.cpp /link /OUT:amsi_bypass.exe /SUBSYSTEM:CONSOLE /MACHINE:x64
-
-del *.obj
+cl.exe /W0 /D_USRDLL /D_WINDLL amsi.cpp /MT /link /DLL /OUT:amsi-bypass.dll
+echo Cleaning up...
+del *.obj *.lib *.exp
